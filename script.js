113ba54e39057df71a693258073f2ca1946c0b90
Darnell GIttens <[darnellgittens16@gmail.com]>
2024-08-20 12:14:12 -0400

    RPS Update

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-
-
+const content = document.querySelector("#comtainer")
+para = document.createElement("p")
 
 function getComputerChoice(){
    if(Math.random() < 0.33){
@@ -25,53 +25,160 @@ function getHumanChoice(){
 }
 
 
+document.addEventListener("DOMContentLoaded", () =>{
+    const rock = document.querySelector("#rock")
 
-function playGame(){
-    let computerScore = 0
-    let humanScore = 0
-
-    function playRound(humanChoice,computerChoice){
-        if(humanChoice == "rock" && computerChoice == "rock"){
-            console.log("It is a tie!")}
-        else if(humanChoice == "rock" && computerChoice =="paper"){
-            console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
-        }
-        else if(humanChoice == "rock" && computerChoice =="scissors"){
-            console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
-        }
-        else if(humanChoice == "paper" && computerChoice =="paper"){
-            console.log("It is a tie!")
-        }
-        else if(humanChoice == "paper" && computerChoice =="rock"){
-            console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
-        }
-        else if(humanChoice == "paper" && computerChoice =="scissors"){
-            console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
-        }
-        else if(humanChoice == "scissors" && computerChoice =="paper"){
-            console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
-        }
-        else if(humanChoice == "scissors" && computerChoice =="rock"){
-            console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
-        }
-        else if(humanChoice == "scissors" && computerChoice =="scissors"){
-            console.log("It is a tie!")
-        }
-        
-    }
-
-
-
-    for(let i = 0; i<5; i++){
-        let HumanSelection = getHumanChoice()
-        let computerSelection = getComputerChoice()
-        playRound(HumanSelection,computerSelection)
-        console.log("The score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+    rock.addEventListener("click", () =>{
+        function playGame(){
+          let computerScore = 0
+          let humanScore = 0
+    
+          function playRound(humanChoice,computerChoice){
+            if(humanChoice == "rock" && computerChoice == "rock"){
+                para.textContent = "It is a tie!"}
+            else if(humanChoice == "rock" && computerChoice =="paper"){
+                para.textContent = `You have lost! ` + `Computer Score: ${computerScore+=1}`
+            }
+            else if(humanChoice == "rock" && computerChoice =="scissors"){
+                para.textContent = `You win! ` + `Human Score: ${humanScore+=1}`
+            }
+            else if(humanChoice == "paper" && computerChoice =="paper"){
+                para.textContent = "It is a tie!"
+            }
+            else if(humanChoice == "paper" && computerChoice =="rock"){
+                para.textContent = `You win! ` + `Human Score: ${humanScore+=1}`
+            }
+            else if(humanChoice == "paper" && computerChoice =="scissors"){
+                para.textContent = `You have lost! ` + `Computer Score: ${computerScore+=1}`
+            }
+            else if(humanChoice == "scissors" && computerChoice =="paper"){
+                para.textContent = `You win! ` + `Human Score: ${humanScore+=1}`
+            }
+            else if(humanChoice == "scissors" && computerChoice =="rock"){
+                para.textContent = `You have lost! ` + `Computer Score: ${computerScore+=1}`
+            }
+            else if(humanChoice == "scissors" && computerChoice =="scissors"){
+                para.textContent = "It is a tie!"
+            }
             
+          }
+    
+         let HumanSelection = "rock"
+         let computerSelection = getComputerChoice()
+         playRound(HumanSelection,computerSelection)
+         console.log("The score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+                
+         return console.log("The final score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
         }
-        return console.log("The final score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
-}
 
-playGame()
+      playGame()
+
+    })
+
+})
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const paper = document.querySelector("#paper")
+
+    paper.addEventListener("click", () =>{
+        function playGame(){
+          let computerScore = 0
+          let humanScore = 0
+    
+          function playRound(humanChoice,computerChoice){
+            if(humanChoice == "rock" && computerChoice == "rock"){
+                console.log("It is a tie!")}
+            else if(humanChoice == "rock" && computerChoice =="paper"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "rock" && computerChoice =="scissors"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "paper" && computerChoice =="paper"){
+                console.log("It is a tie!")
+            }
+            else if(humanChoice == "paper" && computerChoice =="rock"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "paper" && computerChoice =="scissors"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="paper"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="rock"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="scissors"){
+                console.log("It is a tie!")
+            }
+            
+          }
+    
+         let HumanSelection = "paper"
+         let computerSelection = getComputerChoice()
+         playRound(HumanSelection,computerSelection)
+         console.log("The score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+                
+         return console.log("The final score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+        }
+
+      playGame()
+
+    })
+
+})
+
+document.addEventListener("DOMContentLoaded", () =>{
+    const scissors = document.querySelector("#scissors")
+
+    scissors.addEventListener("click", () =>{
+        function playGame(){
+          let computerScore = 0
+          let humanScore = 0
+    
+          function playRound(humanChoice,computerChoice){
+            if(humanChoice == "rock" && computerChoice == "rock"){
+                console.log("It is a tie!")}
+            else if(humanChoice == "rock" && computerChoice =="paper"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "rock" && computerChoice =="scissors"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "paper" && computerChoice =="paper"){
+                console.log("It is a tie!")
+            }
+            else if(humanChoice == "paper" && computerChoice =="rock"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "paper" && computerChoice =="scissors"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="paper"){
+                console.log(`You win! ` + `Human Score: ${humanScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="rock"){
+                console.log(`You have lost! ` + `Computer Score: ${computerScore+=1}`)
+            }
+            else if(humanChoice == "scissors" && computerChoice =="scissors"){
+                console.log("It is a tie!")
+            }
+            
+          }
+    
+         let HumanSelection = "scissors"
+         let computerSelection = getComputerChoice()
+         playRound(HumanSelection,computerSelection)
+         console.log("The score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+                
+         return console.log("The final score is:\n Computer: " + computerScore + "\n Player: " + humanScore);
+        }
+
+      playGame()
+
+    })
+
+})
 
 
